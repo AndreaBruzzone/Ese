06b88941f28ec681be750fd04f0e114f1690dc74
AndreaBruzzone <andrea.bruzzone.93@outlook.it>
2017-03-01 11:30:06 +0100

    New format expression

--- JavaCalc/src/PervasiveElectronicExamples/JavaCalc.java
+++ JavaCalc/src/PervasiveElectronicExamples/JavaCalc.java
@@ -5,32 +5,29 @@ import java.util.Scanner;
 public class JavaCalc {
 
 	public static void main(String[] args) {
-		double n1, n2;
-		String op;
-		Scanner scannerObject = new Scanner(System.in);
-		System.out.println("Enter first number:");
-		n1 = scannerObject.nextDouble();
-		System.out.println("Enter second number:");
-		n2 = scannerObject.nextDouble();
-		Scanner scannerOp = new Scanner(System.in);
-		System.out.println("Enter operation:");
-		op = scannerOp.next();
 
-		switch (op) {
-		case "+":
-			System.out.println("Result = " + (n1 + n2));
-			break;
-		case "-":
-			System.out.println("Result = " + (n1 - n2));
-			break;
-		case "/":
-			System.out.println("Result = " + (n1 / n2));
-			break;
-		case "*":
-			System.out.println("Result = " + (n1 * n2));
-			break;
-		default:
-			break;
+		try (Scanner scanner = new Scanner(System.in)) {
+			System.out.println("Enter an expression of the form 3 * 5:");
+			double n1 = scanner.nextDouble();
+			String op = scanner.next();
+			double n2 = scanner.nextDouble();
+
+			switch (op) {
+			case "+":
+				System.out.println("Result = " + (n1 + n2));
+				break;
+			case "-":
+				System.out.println("Result = " + (n1 - n2));
+				break;
+			case "/":
+				System.out.println("Result = " + (n1 / n2));
+				break;
+			case "*":
+				System.out.println("Result = " + (n1 * n2));
+				break;
+			default:
+				break;
+			}
 		}
 	}
 
