567d00a8dcdf2062b2406724441a45594227d6f4
AndreaBruzzone <andrea.bruzzone.93@outlook.it>
2017-03-01 11:14:39 +0100

    Calculator

--- JavaCalc/src/PervasiveElectronicExamples/JavaCalc.java
+++ JavaCalc/src/PervasiveElectronicExamples/JavaCalc.java
@@ -1,10 +1,37 @@
 package PervasiveElectronicExamples;
 
+import java.util.Scanner;
+
 public class JavaCalc {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		double n1, n2;
+		String op;
+		Scanner scannerObject = new Scanner(System.in);
+		System.out.println("Enter first number:");
+		n1 = scannerObject.nextDouble();
+		System.out.println("Enter second number:");
+		n2 = scannerObject.nextDouble();
+		Scanner scannerOp = new Scanner(System.in);
+		System.out.println("Enter operation:");
+		op = scannerOp.next();
+
+		switch (op) {
+		case "+":
+			System.out.println("Result = " + (n1 + n2));
+			break;
+		case "-":
+			System.out.println("Result = " + (n1 - n2));
+			break;
+		case "/":
+			System.out.println("Result = " + (n1 / n2));
+			break;
+		case "*":
+			System.out.println("Result = " + (n1 * n2));
+			break;
+		default:
+			break;
+		}
 	}
 
 }
