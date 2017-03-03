41a6c86ae116858f054d571848ac365f7cf8ff39
AndreaBruzzone <andrea.bruzzone.93@outlook.it>
2017-03-03 18:00:56 +0100

    up

--- JavaCalc/src/PervasiveElectronicExamples/JavaCalc.java
+++ JavaCalc/src/PervasiveElectronicExamples/JavaCalc.java
@@ -6,7 +6,7 @@ public class JavaCalc {
 
 	public static void main(String[] args) {
 		try (Scanner scanner = new Scanner(System.in)) {
-			System.out.println("Enter an expression of the form 3 * 5:");
+			System.out.println("Enter an expression of the form -> 3 * 5:");
 			double n1 = scanner.nextDouble();
 			String op = scanner.next();
 			double n2 = scanner.nextDouble();
