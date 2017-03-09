897bd6a950f0748b1e085d5df1995e83a5ecb9c8
AndreaBruzzone <andrea.bruzzone.93@outlook.it>
2017-03-09 13:53:30 +0100

    refresh

--- MoneyExe/src/Pervasive/Sample/IMoney.java
+++ MoneyExe/src/Pervasive/Sample/IMoney.java
@@ -1,45 +1,37 @@
 package Pervasive.Sample;
 
 public interface IMoney {
-	/**
-	 * Adds a money to this money.
-	 */
+	// Adds a money to this money.
+
 	public abstract IMoney add(IMoney m);
 
-	/**
-	 * Adds a simple Money to this money. This is a helper method for
-	 * implementing double dispatch
-	 */
+	// Adds a simple Money to this money. This is a helper method for
+	// implementing double dispatch
+
 	public abstract IMoney addMoney(Money m);
 
-	/**
-	 * Adds a MoneyBag to this money. This is a helper method for implementing
-	 * double dispatch
-	 */
+	// Adds a MoneyBag to this money. This is a helper method for implementing
+	// double dispatch
+
 	public abstract IMoney addMoneyBag(MoneyBag s);
 
-	/**
-	 * Tests whether this money is zero
-	 */
+	// Tests whether this money is zero
+
 	public abstract boolean isZero();
 
-	/**
-	 * Multiplies a money by the given factor.
-	 */
+	// Multiplies a money by the given factor.
+
 	public abstract IMoney multiply(int factor);
 
-	/**
-	 * Negates this money.
-	 */
+	// Negates this money.
+
 	public abstract IMoney negate();
 
-	/**
-	 * Subtracts a money from this money.
-	 */
+	// Subtracts a money from this money.
+
 	public abstract IMoney subtract(IMoney m);
 
-	/**
-	 * Append this to a MoneyBag m.
-	 */
+	// Append this to a MoneyBag m.
+
 	public abstract void appendTo(MoneyBag m);
 }
